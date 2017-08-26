2e38fdab0bb8868f2b6bacbb9b2153e5c14139f0
John Huang <johnhuang1225@gmail.com>
2017-08-26 22:24:26 +0800

    14.文章詳情頁(上) 1.路由開發 2.SEO 支持 3.模版設計 4. 點讚計數功能

--- app/controllers/blog/post.js
+++ app/controllers/blog/post.js
@@ -63,7 +63,55 @@ router.get('/category/:name', function (req, res, next) {
 
 });
 
-router.get('/view', function (req, res, next) {});
+router.get('/view/:id', function (req, res, next) {
+  if (!req.params.id) {
+    return next(new Error('no post id provided'));
+  }
+  // 同時支持id與slug
+  var condition = {};
+  try {
+    condition._id = mongoose.Types.ObjectId(req.params.id);
+  } catch (error) {
+    condition.slug = req.params.id;
+  }
+  Post.findOne(condition)
+      .populate('category')
+      .populate('author')
+      .exec(function(err, post) {
+        if (err) {
+          next(err);
+        }
+        res.render('blog/view', {
+          post: post
+        });
+      });
+});
+
+router.get('/favorites/:id', function (req, res, next) {
+  if (!req.params.id) {
+    return next(new Error('no post id provided'));
+  }
+  // 同時支持id與slug
+  var condition = {};
+  try {
+    condition._id = mongoose.Types.ObjectId(req.params.id);
+  } catch (error) {
+    condition.slug = req.params.id;
+  }
+  Post.findOne(condition)
+      .populate('category')
+      .populate('author')
+      .exec(function(err, post) {
+        if (err) {
+          next(err);
+        }
+        post.meta.favorites = post.meta.favorites ? post.meta.favorites + 1 : 1;
+        post.markModified('meta');
+        post.save(function(err) {
+          res.redirect('/posts/view/' + post.slug);
+        });
+      });
+});
 
 router.get('/comment', function (req, res, next) {});
 
